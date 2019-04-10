9b1851a019cdc56d6b599991daf49b17548dbfa2
89dean <dean.february3@gmail.com>
2019-04-10 19:55:15 +0200

    Initial commit

---
+++ src/test/java/com/dean/factory/RegistrationFormFactoryTest.java
@@ -0,0 +1,17 @@
+package com.dean.factory;
+
+import com.dean.domain.RegistrationForm;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegistrationFormFactoryTest {
+
+    @Test
+    public void getRegistrationForm() {
+
+
+        RegistrationForm r = RegistrationFormFactory.getRegistrationForm("Dean","February","dean.february3@gmail.com","Running");
+        System.out.println(r);
+        Assert.assertNotNull(r.getName());
+    }
+}
