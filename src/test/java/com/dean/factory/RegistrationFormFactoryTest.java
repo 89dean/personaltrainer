85bcc904ad3999069179245edf6a36d3e88b04f4
89dean <dean.february3@gmail.com>
2019-04-14 21:42:00 +0200

    quarter way

--- src/test/java/com/dean/factory/RegistrationFormFactoryTest.java
+++ src/test/java/com/dean/factory/RegistrationFormFactoryTest.java
@@ -9,7 +9,6 @@ public class RegistrationFormFactoryTest {
     @Test
     public void getRegistrationForm() {
 
-
         RegistrationForm r = RegistrationFormFactory.getRegistrationForm("Dean","February","dean.february3@gmail.com","Running");
         System.out.println(r);
         Assert.assertNotNull(r.getName());
